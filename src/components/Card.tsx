9a26f5bc604269da9f4690a6293f35a4fb7cd495
OYaagoub <oyaagoub@sescam.jccm.es>
2024-04-16 17:22:51 +0200

    UI-T4 Create Home page

---
+++ src/components/Card.tsx
@@ -0,0 +1,53 @@
+function Card(movie: any) {
+    return (
+
+        <div className="py-3 sm:max-w-xl sm:mx-auto mb-[80px]">
+            <div className="bg-white shadow-lg border-gray-100 max-h-80	 border sm:rounded-3xl p-8 flex space-x-8">
+                <div className="h-48 overflow-visible w-1/2">
+                    <img className="rounded-3xl shadow-lg" src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1LRLLWGvs5sZdTzuMqLEahb88Pc.jpg" alt="" />
+                </div>
+                <div className="flex flex-col w-1/2 space-y-4">
+                    <div className="flex justify-between items-start">
+                        <h2 className="text-3xl font-bold dark:text-gray-900">Sweet Tooth: El niño ciervo</h2>
+                        {/* <div className="bg-yellow-400 font-bold rounded-xl p-2">7.2</div> */}
+
+
+
+
+                    </div>
+                    <div>
+                        <div className="text-sm text-gray-400">Movies</div>
+                        <div className="text-lg text-gray-800">2019</div>
+                    </div>
+                    <p className=" text-gray-400 min-h-[18px] max-h-40 overflow-y-hidden"></p>
+
+                    <div className="flex items-center" title="Rate now">
+                        <svg  className="w-10 h-10 text-yellow-300 me-1 hover:scale-125 transform-gpu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg className="w-10 h-10 text-yellow-300 me-1 hover:scale-125 transform-gpu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg className="w-10 h-10 text-yellow-300 me-1 hover:scale-125 transform-gpu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg className="w-10 h-10 text-yellow-300 me-1 hover:scale-125 transform-gpu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg className="w-10 h-10 text-gray-300 me-1 dark:text-gray-500 hover:scale-125 transform-gpu" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">4.95</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    );
+}
+
+export default Card;
